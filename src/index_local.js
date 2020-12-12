d2f490272d15a84b8011aef1bc59305c011a4500
mahongquan <mahongquan@sina.com>
2020-12-12 15:14:34 +0800

    h

--- src/index_local.js
+++ src/index_local.js
@@ -1,1 +1,5 @@
-require("./main");
+import React from 'react';
+import ReactDOM from 'react-dom';
+let module_name = './editor';
+let App = require(module_name).default;
+ReactDOM.render(<App />, document.getElementById('root'));
